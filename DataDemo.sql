38aa7811309223d72f07b3c830035e08aff69e97
blackjackVN <88996201+blackjackVN@users.noreply.github.com>
2023-10-19 20:37:50 +0700

    Add files via upload

---
+++ DataDemo.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE productlist (
+    productid INT,
+    productname VARCHAR(255),
+	unitinstock int
+);
+
+INSERT INTO productlist (productid, productname,unitinstock)
+VALUES (1, N' Bánh mì',40),
+       (2, N' Bánh bao',20),
+       (3, N' Bánh tráng',15);
+
+
+CREATE TABLE categories (
+    productid INT,
+    productname VARCHAR(255),
+
+);
+	   INSERT INTO categories (productid, productname)
+VALUES (1, N' Bánh'),
+       (2, N' Kẹo')
+   
